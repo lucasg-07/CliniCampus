5a2a569ce9d3af9a4a5d74bc52f7d43b83e97c37
lucasg-07 <gonlucas@outlook.com>
2024-08-16 15:27:12 -0300

    Alteracao tela marcar consulta

--- clinicampus/ConsultasView.swift
+++ clinicampus/ConsultasView.swift
@@ -8,11 +8,6 @@ import SwiftUI
 
 struct ConsultasView: View {
     var body: some View {
-<<<<<<< HEAD
-        Text("AGUA")
-=======
         Text("Marcar consultas")
-        
->>>>>>> 53d32df90f0f58fe51e82aa68466f6801ee629bb
     }
 }
