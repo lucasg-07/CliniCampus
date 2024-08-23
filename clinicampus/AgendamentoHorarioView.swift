cf1810c8c36711d6f685b55a2fbab8629b011336
lucasg-07 <gonlucas@outlook.com>
2024-08-23 14:55:35 -0300

    updates tela-ha

--- clinicampus/AgendamentoHorarioView.swift
+++ clinicampus/AgendamentoHorarioView.swift
@@ -9,72 +9,87 @@ import SwiftUI
 
 struct AgendamentoHorarioView: View {
     
+    @State private var selectedDate = Date()
+    @State private var selectedHorario: String?
+    @State private var showingDatePicker = false
+    
     let oftalmologistas: [Oftalmologista] = [
         Oftalmologista(nome: "Dr. Osvaldo Junior", dia: "Qua 03/04", horarios: ["10:00", "13:00", "16:00"]),
-    Oftalmologista(nome: "Dr. Irineu Machado", dia: "Qua 03/04", horarios: ["07:30", "13:30", "16:30"]),
-    Oftalmologista(nome: "Dra. Marcelle Pinheiro", dia: "Qui 04/04", horarios: ["08:30", "09:30", "14:00"])
-]
+        Oftalmologista(nome: "Dr. Irineu Machado", dia: "Qua 03/04", horarios: ["07:30", "13:30", "16:30"]),
+        Oftalmologista(nome: "Dra. Marcelle Pinheiro", dia: "Qui 04/04", horarios: ["08:30", "09:30", "14:00"])
+    ]
     var body: some View {
-        
         NavigationView {
-                    VStack {
+            VStack {
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        showingDatePicker.toggle()
+                    }) {
                         HStack {
-                            Spacer()
-                            Button(action: {
-                            
-                            }) {
-                                HStack {
-                                    Text("Alterar Datas")
-                                    Image(systemName: "calendar")
-                                }
-                            }
-                            .padding(.trailing)
+                            Text("Alterar Datas")
+                            Image(systemName: "calendar")
                         }
-                        .padding(.top)
-                        
-                        List(oftalmologistas) { oftalmologista in
-                            Section(header: Text(oftalmologista.nome)
-                                        .font(.headline)
-                                        .padding(.horizontal, 5)) {
-                                Text(oftalmologista.dia)
-                                    .font(.subheadline)
-                                    .foregroundColor(.gray)
-                                
-                                HStack {
-                                    ForEach(oftalmologista.horarios, id: \.self) { horario in
-                                        Button(action: {
-                                            // Ação para selecionar o horário
-                                        }) {
-                                            Text(horario)
-                                                .padding()
-                                                .background(Color.red)
-                                                .foregroundColor(.white)
-                                                .cornerRadius(10)
-                                        }
+                    }
+                    .padding(.trailing)
+                }
+                .padding(.top)
+                
+                List(oftalmologistas) { oftalmologista in
+                    Section(header: Text(oftalmologista.nome)
+                        .font(.headline)
+                        .padding(.vertical, 5)) {
+                            Text(oftalmologista.dia)
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                            
+                            HStack {
+                                ForEach(oftalmologista.horarios, id: \.self) { horario in
+                                    Button(action: {
+                                        selectedHorario = horario
+                                    }) {
+                                        Text(horario)
+                                            .padding()
+                                            .background(selectedHorario == horario ? Color.blue: Color.red)
+                                            .foregroundColor(.white)
+                                            .cornerRadius(10)
                                     }
                                 }
-                                .padding(.vertical)
                             }
+                            .padding(.vertical)
                         }
-                        .listStyle(GroupedListStyle())
-                        .navigationTitle("Horários de Oftalmologistas")
                     }
+                .listStyle(GroupedListStyle())
+                .navigationTitle("Horários de Oftalmologistas")
+                .navigationBarTitleDisplayMode(.inline)
+            }
+            .sheet(isPresented: $showingDatePicker) {
+                DatePicker("Selecionar Data", selection: $selectedDate, displayedComponents: .date)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                    .padding()
+                
+                Button("Confirmar") {
+                    
+                    showingDatePicker = false
                 }
+                .padding()
             }
         }
-
-        struct Oftalmologista: Identifiable {
-            let id = UUID()
-            let nome: String
-            let dia: String
-            let horarios: [String]
-        }
-
-        
-    
-
-
-#Preview {
-        AgendamentoHorarioView()
     }
-
+}
+    struct Oftalmologista: Identifiable {
+        let id = UUID()
+        let nome: String
+        let dia: String
+        let horarios: [String]
+    }
+    
+    
+    
+    
+    struct MainView_Previews: PreviewProvider {
+        static var previews: some View {
+            AgendamentoHorarioView()
+    }
+    
+}
